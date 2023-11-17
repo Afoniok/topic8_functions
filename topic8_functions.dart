1b35956cb8fe378739cf01105fa1af7a5a4b46b4
Afoniok <120436963+Afoniok@users.noreply.github.com>
2023-11-17 13:03:02 +0600

    Add files via upload

---
+++ topic8_functions.dart
@@ -0,0 +1,107 @@
+// void main() {
+//   pechat();
+//   String childnale = child();
+//   print(childnale);
+//   String adges = ages(4);
+//   print(adges);
+// }
+
+// void pechat() => print("detdom");
+// String child() => "Adil";
+// String ages(int age) => "HE's age is $age";
+// void pechat() {
+//   print("detdom");
+// }
+
+// String child() {
+//   return "Diana";
+// }
+
+// String ages(int age) {
+//   return "He's age is $age";
+// }
+
+// void main() {
+//   info(name: "Anton", age: 33);
+// }
+
+// void info({name, age}) {
+//   print("My name is $name and my age is $age");
+// }
+
+// void addNum(a, b) {
+//   print(a + b);
+// }
+
+// void main() {
+//   addNum(2, 5);
+// }
+
+//1
+
+// void main() {
+//   tree(6, 34, 0);
+// }
+
+// void tree(a, b, c) {
+//   int maxx = max(a, b);
+//   int maxxx = max(maxx, c);
+//   print(maxxx);
+// }
+
+//2
+// void main() {
+//   summ(3, 78, 2, 98, 2);
+// }
+
+// void summ(a, b, c, d, e) {
+//   int summm = a + b + c + d + e;
+//   print(summm);
+// }
+
+//3
+// void main() {
+//   mult(8, 2, 3, -1, 7);
+// }
+
+// void mult(a, b, c, d, e) {
+//   var tort = a * b * c * d * e;
+//   print(tort);
+// }
+
+//4
+// void main() {
+//   ji("1234Abcd");
+//   print(ji("1234Abcd"));
+// }
+
+// String ji(String neww) {
+//   List<String> letters = neww.split('');
+//   letters = letters.reversed.toList();
+//   return letters.join();
+// }
+
+//5
+
+// void main() {
+//   fact(5);
+// }
+
+// void fact(int numberr) {
+//   int sum = 1;
+//   for (int i = 1; i <= numberr; i++) {
+//     sum = sum * i;
+//   }
+//   print(sum);
+// }
+
+
+//8
+
+// void main() {
+//   baby({1, 2, 3, 3, 3, 3, 4, 5});
+// }
+
+// void baby(Set ele) {
+//   print(ele);
+// }
